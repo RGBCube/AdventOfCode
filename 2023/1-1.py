4ecd58f31e34c6c5a55538186da7c4ca5ac09d70
RGBCube <RGBCube@users.noreply.github.com>
2023-12-02 00:24:55 +0300

    Compress python 1-1

--- 2023/1-1.py
+++ 2023/1-1.py
@@ -1,1 +1,1 @@
-print(sum(int(__import__("functools").reduce(lambda x,y: x+y,(lambda a:[a[0],a[-1]])(list(filter(lambda c:c.isdigit(),l)))))for l in __import__("pathlib").Path("1-1.in").read_text().splitlines()))
+print(sum(int("".join((lambda a:[a[0],a[-1]])(list(filter(lambda c:c.isdigit(),l)))))for l in __import__("pathlib").Path("1-1.in").read_text().splitlines()))
